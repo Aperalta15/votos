bb696cbd536084990cee9be975f1ccfbb29c92d0
DarkSnow15 <antperaltin@gmail.com>
2019-07-11 08:44:38 -0500

    Model Persona
    Relacion entre user y persona

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_051223) do
+ActiveRecord::Schema.define(version: 2019_07_11_132355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "personas", force: :cascade do |t|
+    t.integer "cedula"
+    t.string "nombre"
+    t.string "apellido"
+    t.string "celular"
+    t.string "direccion"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_personas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_07_11_051223) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "personas", "users"
 end
