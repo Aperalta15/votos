48c00f8d30c99466da311c3cb3e80fb94b9cec8a
DarkSnow15 <antperaltin@gmail.com>
2019-07-13 18:20:35 -0500

    Datos de usuario agregado

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,3 +1,8 @@
 class Persona < ApplicationRecord
   belongs_to :user
+
+  validates :cedula, presence: true
+  validates :nombre, presence: true
+  validates :apellido, presence: true
+  validates :direccion, presence: true
 end
